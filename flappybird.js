076156a922eb11bacbf949806096ca05e14fc480
jusernamepump <168264570+jusernamepump@users.noreply.github.com>
2024-04-28 11:15:00 +0300

    Update flappybird.js

--- flappybird.js
+++ flappybird.js
@@ -63,6 +63,7 @@ window.onload = function() {
     requestAnimationFrame(update);
     setInterval(placePipes, 1500); //every 1.5 seconds
     document.addEventListener("keydown", moveBird);
+    document.addMouseListener("mouseClicked", moveBird);
 }
 
 function update() {
